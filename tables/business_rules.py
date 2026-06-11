02a8fd9db71987a14015b8f26aed7f1bc465a9de
Tim Wang <tianxun.wang1@nhs.net>
2026-06-11 11:55:21 +0100

    added complex rules

--- tables/business_rules.py
+++ tables/business_rules.py
@@ -2,18 +2,35 @@ from sqlalchemy import ForeignKey
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
-class BusinessRules(DeclarativeBase):
-    __tablename__ = 'business_rules'
+class ComplexRule(DeclarativeBase):
+    __tablename__ = 'complex_rule'
 
-    business_rule_id: Mapped[str] = mapped_column('business_rule_id', primary_key=True)
-    business_rule_name: Mapped[str]
-    business_rule_expression: Mapped[str]
+    complex_rule_id: Mapped[str] = mapped_column('complex_rule_id', primary_key=True)
+    complex_rule_name: Mapped[str]
+    complex_rule_expression: Mapped[str]
 
 
-class BusinessRuleAvailableFields(DeclarativeBase):
-    __tablename__ = 'business_rule_available_fields'
+class ComplexRuleAvailableField(DeclarativeBase):
+    __tablename__ = 'complex_rule_available_field'
 
-    business_rule_field_id: Mapped[str] = mapped_column('business_rule_field_id', primary_key=True)
-    business_rule_id: Mapped[str] = mapped_column('business_rule_id', primary_key=True)
-    business_rule_field_names: Mapped[list[str]]
-    business_rule_field_type: Mapped[str]
+    complex_rule_field_id: Mapped[str] = mapped_column('complex_rule_field_id', primary_key=True)
+    complex_rule_id: Mapped[str] = mapped_column('complex_rule_id', primary_key=True)
+    complex_rule_field_name: Mapped[str]
+    complex_rule_field_type: Mapped[str]
+
+
+class CollectionBusinessRule(DeclarativeBase):
+    __tablename__ = 'collection_business_rule'
+
+    collection_business_rule_id: Mapped[str] = mapped_column('collection_business_rule_id', primary_key=True)
+    collection_id: Mapped[str] = mapped_column('collection_id')
+    complex_rule_id: Mapped[str] = mapped_column('complex_rule_id')
+
+
+class CollectionBusinessRuleField(DeclarativeBase):
+    __tablename__ = 'collection_business_rule_field'
+
+    pk: Mapped[str] = mapped_column('pk', primary_key=True)
+    collection_business_rule_id: Mapped[str]
+    complex_rule_field_id: Mapped[str]
+    field_id: Mapped[str]
